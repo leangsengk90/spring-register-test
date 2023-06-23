aaa7651e6baad0100ce1fba05219c5c27543ddcc
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-06-24 01:39:28 +0700

    Update helm-chart.sh

--- kubernetes/helm-chart.sh
+++ kubernetes/helm-chart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar -xvf register-app-prod-0.0.2.tgz
+tar -xvf /opt/kubernetes/register-app-prod-0.0.2.tgz
 helm upgrade register-app-prod /opt/kubernetes/register-app-prod --values=/opt/kubernetes/register-app-prod/values-prod.yaml --install
