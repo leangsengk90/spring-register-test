09fbf39dc34291a7b0153d3cc1f5d64670a92ebb
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2023-06-24 17:21:14 +0700

    Update helm-chart.sh

--- kubernetes/helm-chart.sh
+++ kubernetes/helm-chart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar -xvf /opt/kubernetes/register-app-staging-0.0.100.tgz -C /opt/kubernetes
-helm upgrade --install register-app-staging /opt/kubernetes/register-app-staging --values=/opt/kubernetes/register-app-staging/values-staging.yaml 
+tar -xvf /opt/kubernetes/register-app-$1-$2.tgz -C /opt/kubernetes
+helm upgrade --install register-app-$1 /opt/kubernetes/register-app-$1 --values=/opt/kubernetes/register-app-$1/values-$1.yaml 
